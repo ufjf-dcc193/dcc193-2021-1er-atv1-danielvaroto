6f6f1bf2478d678043d4d89430c819793ef974cc
Daniel Varoto <daniel.varoto@loft.com.br>
2021-05-20 21:33:00 -0300

    Add an empty spring boot project

---
+++ src/main/java/br/ufjf/dcc193/atv1/Atv1Application.java
@@ -0,0 +1,14 @@
+package br.ufjf.dcc193.atv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atv1Application.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/br/ufjf/dcc193/atv1/Atv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.atv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
